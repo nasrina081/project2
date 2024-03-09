b20b9a22b1b6bd26cd321e5c793c6c9caa3474fc
Nasrin Ali <53440444+nasrina081@users.noreply.github.com>
2024-03-09 18:52:13 -0500

    starting
    initialization

---
+++ flashcards/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Flower Power: Discover Different Blooms 🌺</h2>
+      <h4>How many different types of flowers do you know? Test yourself and learn some more here!</h4>
+      <h5>Number of cards: 10</h5>
+    </div>
+  )
+
+}
+export default App
