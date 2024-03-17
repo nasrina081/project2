d3118447aa36c483f1c2e5cb4ffd2c2664c569b2
Nasrin Ali <53440444+nasrina081@users.noreply.github.com>
2024-03-17 00:19:46 -0400

    ??

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -1,18 +1,35 @@
 import { useState } from 'react'
-import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/Card'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App"> 
-      <h2>Flower Power: Discover Different Blooms 🌺</h2>
-      <h4>How many different types of flowers do you know? Test yourself and learn some more here!</h4>
-      <h5>Number of cards: 10</h5>
-
-      <Card />
-    </div>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
-
 }
+
 export default App
